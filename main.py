0fce3f02adcdfc5ae646a8788662be211485c3f2
JEnriquePM <jenriqueplaza@gmial.com>
2022-12-04 22:25:58 +0100

    Style in base template

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ async def images(request: Request, text: str = Form(), settings: config.Settings
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv(
-        "PORT", default=5000), log_level="info")
+        "PORT", default=8000), log_level="info")
